0b7b099811de0af0be06b61b71c23bb95c19eaa3
Jozef <jozefw@sbcglobal.net>
2014-05-03 13:19:30 -0700

    wever

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwearJar::Application.config.secret_key_base = '7f683d70eae08800a5e529ce8cb5df251726f1303d31388af3532d19e516419f46e38fa66d2c556063e928801667b38a91aeb3b3cfb613d8322f7b9988819678'
