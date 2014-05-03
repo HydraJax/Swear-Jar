b88917ebe41143ba5810339490ca1b3e86a9a1f9
Jozef <jozefw@sbcglobal.net>
2014-05-03 13:58:34 -0700

    seed models and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: "joe", password: "ohboy", email: "joe@gmail.com")
+User.create(name: "Heren", password: "rotsaruck", email: "reerry@gmail.com")
